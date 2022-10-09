9a2cb46d97a9f42b56701c887663dce4f934e45b
Kunal D <kunaldudhe@gmail.com>
2022-10-08 23:43:19 -0400

    Update server.erl
    Updated server.erl

--- Project 2/server.erl
+++ Project 2/server.erl
@@ -1,30 +1,67 @@
 -module(server).
 % -import(, []).
--export([start/0, print/0]).
+-export([start/0, initiate/2,initiate/1]).
 
 start() ->
-    % PID = spawn(server, print, []),
-    % PID2 = spawn(server, print, []),
-    PIDList = [],
-    register('PID1', spawn(server, print, [])),
-    PIDList = lists:append(PIDList, ['PID1']),
-    register('PID2', spawn(server, print, [])),
-    register('PID3', spawn(server, print, [])),
-    io:format("~p~n", [registered()]),
-    io:format("~p~n", [length(registered())]),
-    io:format("~p~n", [self()]),
-    io:format("~p~n", [processes()]).
-% PID ! printThisShit,
+    register('actor1', spawn(server, initiate, ["This is a rumor",1])),
+    io:format("Initiating a rumor~n"),
+    register('actor2', spawn(server, initiate, [1])),
+    register('actor3', spawn(server, initiate, [1])),
+    'actor1' ! receiveRumour.
 % PID ! printThisShit,
 % PID2 ! sendMSG.
 
-print() ->
+
+initiate(Count) ->
     receive
-        printThisShit ->
-            io:format("Started an actor~n", []),
-            'PID3' ! printThisShit,
-            io:format("After PID3~p~n", [self()]),
-            print();
-        sendMSG ->
-            io:format("Second print~n", [])
+        receiveRumour ->
+            io:format("Received ~n",[]),
+            io:format("~p Count ~n",[Count]),
+            % 'actor1' ! receiveRumour,
+            % count=count+1,
+            if
+                Count==10->
+                    % self ! stop;
+                    "";
+                true->
+                    % self ! receiveRumour
+                    'actor1' ! receiveRumour
+                    
+            end,
+            initiate(Count+1);
+        stop ->
+            io:format("~p Stopping~n",[self()]),
+            'actor1' ! stop
     end.
+
+initiate(Rumor,Count) ->
+    receive
+        receiveRumour ->
+            io:format("Received a rumor ~n",[]),
+            io:format("~p Count parent~n",[Count]),
+            if
+                Count==10->
+                    % self ! stop;
+                    "";
+                true->
+                    % self ! receiveRumour
+                    'actor2' ! receiveRumour,
+                    'actor3' ! receiveRumour
+            end,
+            initiate(Rumor,Count+1);
+        stop ->
+            io:format("~p Stopping~n",[self()]),
+            'actor2' ! stop,
+            'actor3' ! stop
+    end.
+
+
+
+% count(Cnt)->
+%     if
+%         Cnt==10->
+%             io:format("terminating ~n",[]);
+%         true->
+%             io:format("~p ~n",[Cnt]),
+%             count(Cnt+1)
+%     end.
