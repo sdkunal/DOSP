47a74eae66d41f373037da65888d1b0690aad5d3
Kunal D <kunaldudhe@gmail.com>
2022-10-10 00:47:01 -0400

    Update server.erl
    Working Full Network topology

--- Project 2/server.erl
+++ Project 2/server.erl
@@ -1,59 +1,102 @@
 -module(server).
-% -import(, []).
--export([start/0, initiate/2,initiate/1]).
+-import(lists, [nth/2]).
+-export([start/1, initiate/2, processKiller/1,add/1,createList/2,append/3,spreadRumor/5,randomize/5]).
 
-start() ->
-    register('actor1', spawn(server, initiate, ["This is a rumor",1])),
-    io:format("Initiating a rumor~n"),
-    register('actor2', spawn(server, initiate, [1])),
-    register('actor3', spawn(server, initiate, [1])),
-    'actor1' ! receiveRumour.
-% PID ! printThisShit,
-% PID2 ! sendMSG.
+start(NumNodes) ->
+    List=createList(1,NumNodes),
+    RandList = [rand:uniform(NumNodes) || _ <- lists:seq(1, NumNodes div NumNodes)],
+    io:format("~p randomNum: ~n",[nth(1,RandList)]),
+    Curr=nth(1,RandList),
+    randomize(RandList,1,List,NumNodes,Curr).
+    
+randomize(RandList,Index,List,NumNodes,Curr)->
+    case Index=<length(RandList) of
+        true->
+            nth(Curr,List) ! {receiveRumour,List,NumNodes},
+            % io:format("hello~n",[]),
+            Curr2=nth(Index+1,RandList),
+            randomize(RandList,Index+1,List,NumNodes,Curr2);
+        false->
+            ""
+    end.
 
+    
 
-initiate(Count) ->
-    receive
-        receiveRumour ->
-            io:format("Received ~n",[]),
-            io:format("~p Count ~n",[Count]),
-            % 'actor1' ! receiveRumour,
-            % count=count+1,
-            if
-                Count==10->
-                    % self ! stop;
-                    "";
-                true->
-                    % self ! receiveRumour
-                    'actor1' ! receiveRumour
-                    
-            end,
-            initiate(Count+1);
-        stop ->
-            io:format("~p Stopping~n",[self()]),
-            'actor1' ! stop
+createList(S,E)->
+    append(S,E,[]).
+
+append(S,E,L)->
+    case S=<E of
+        true ->
+            append(S+1,E,lists:append([L,[spawn(server,initiate,["This is a rumor",1])]]));
+        false ->
+            L
     end.
 
 initiate(Rumor,Count) ->
     receive
-        receiveRumour ->
-            io:format("Received a rumor ~n",[]),
-            io:format("~p Count parent~n",[Count]),
+        {receiveRumour,List,NumNodes} ->
+            io:format("~p ~p ~n",[self(),Rumor]),
+            io:format("~p Count: ~n",[Count]),
             if
-                Count==10->
-                    % self ! stop;
-                    "";
+                Count==5->
+                    processKiller(self());
                 true->
-                    % self ! receiveRumour
-                    'actor2' ! receiveRumour,
-                    'actor3' ! receiveRumour
-            end,
-            initiate(Rumor,Count+1);
+                    spreadRumor(length(List),NumNodes,List,Rumor,self()),
+                    initiate(Rumor,Count+1)
+                    
+            end;
         stop ->
-            io:format("~p Stopping~n",[self()]),
-            'actor2' ! stop,
-            'actor3' ! stop
+            io:format("~p Stopping~n",[self()])
     end.
+
+spreadRumor(Nodes,NumNodes,List,Rumor,CurrPID)->
+    case NumNodes>0 of
+        true->
+            case checkIfEqual(CurrPID,nth(NumNodes,List)) of
+                true->
+                    spreadRumor(Nodes,NumNodes-1,List,Rumor,CurrPID);
+                false->
+                    nth(NumNodes,List) ! {receiveRumour,List,Nodes},
+                    spreadRumor(Nodes,NumNodes-1,List,Rumor,CurrPID)
+            end;
+        false->
+            ""
+    end.
+
+checkIfEqual(PID1,PID2)->
+    if
+        PID1==PID2 ->
+            true;
+        true->
+            false
+    end.
+
+processKiller(PIDToKill) ->
+    exit(PIDToKill, kill).
+
+
+% initiate(Count) ->
+%     receive
+%         receiveRumour ->
+%             io:format("~p Received ~n",[self()]),
+%             io:format("~p Count ~n",[Count]),
+%             if
+%                 Count==5->
+%                     processKiller(self()),
+%                     initiate(Count);
+%                 true->
+%                     'actor1' ! receiveRumour,
+%                     initiate(Count+1)
+%             end;
+%         stop ->
+%             io:format("~p Stopping~n",[self()]),
+%             exit('actor1',kill),
+%             exit(self(),kill)
+%     end.
+
+
+
 
 
 
@@ -65,3 +108,9 @@ initiate(Rumor,Count) ->
 %             io:format("~p ~n",[Cnt]),
 %             count(Cnt+1)
 %     end.
+
+
+
+
+add(List)->
+    io:format("~p hello~n",[nth(1,List)]).
