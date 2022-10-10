f800bd95b49f990d3cd6d776a43193337609660f
Aakash Anil Naik <89472838+aakash-anil-naik@users.noreply.github.com>
2022-10-10 15:29:01 -0400

    Update server.erl

--- Project 2/server.erl
+++ Project 2/server.erl
@@ -1,116 +1,210 @@
 -module(server).
 -import(lists, [nth/2]).
--export([startFull/1, createFullList/2, spawnFull/3, initiateFull/2, spreadFullRumor/5, randomize/5,
-         startLine/1, createLineList/2, spawnLine/3, initiateLine/2, spreadLineRumor/4,
-         processKiller/1, checkIfEqual/2]).
+-export([
+    startFull/1,
+    createFullList/2,
+    spawnFull/3,
+    initiateFull/2,
+    spreadFullRumor/5,
+    randomize/5,
+    startLine/1,
+    createLineList/2,
+    spawnLine/3,
+    initiateLine/2,
+    spreadLineRumor/2,
+    start2D/2,
+    create2DList/3,
+    spawn2D/4,
+    initiate2D/3,
+    spread2DRumor/3,
+    processKiller/1,
+    checkIfEqual/2
+]).
 
 startFull(NumNodes) ->
-    List=createFullList(1,NumNodes),
+    List = createFullList(1, NumNodes),
     RandList = [rand:uniform(NumNodes) || _ <- lists:seq(1, NumNodes div 10)],
-    Curr=nth(1,RandList),
-    randomize(RandList,1,List,NumNodes,Curr).
+    Curr = nth(1, RandList),
+    randomize(RandList, 1, List, NumNodes, Curr).
 
-randomize(RandList,Index,List,NumNodes,Curr)->
-    case Index=<length(RandList) of
-        true->
-            nth(Curr,List) ! {receiveRumour,List,NumNodes},
-            Curr2=nth(Index+1,RandList),
-            randomize(RandList,Index+1,List,NumNodes,Curr2);
-        false->
+randomize(RandList, Index, List, NumNodes, Curr) ->
+    case Index =< length(RandList) of
+        true ->
+            nth(Curr, List) ! {receiveRumour, List, NumNodes},
+            Curr2 = nth(Index + 1, RandList),
+            randomize(RandList, Index + 1, List, NumNodes, Curr2);
+        false ->
             ""
     end.
 
 startLine(NumNodes) ->
-    List=createLineList(1,NumNodes),
-    nth(1,List) ! {receiveRumour,List,NumNodes}.
+    List = createLineList(1, NumNodes),
+    nth(1, List) ! {receiveRumour, List}.
 
-createFullList(S,E)->
-    spawnFull(S,E,[]).
+start2D(NumNodes, NumCols) ->
+    List = create2DList(1, NumNodes, NumCols),
+    nth(1, List) ! {receiveRumour, List}.
 
-createLineList(S,E)->
-    spawnLine(S,E,[]).
+createFullList(S, E) ->
+    spawnFull(S, E, []).
 
-spawnFull(S,E,L)->
-    case S=<E of
+createLineList(S, E) ->
+    spawnLine(S, E, []).
+
+create2DList(S, E, NumCols) ->
+    spawn2D(S, E, NumCols, []).
+
+spawnFull(S, E, L) ->
+    case S =< E of
         true ->
-            spawnFull(S+1,E,lists:append([L,[spawn(server,initiateFull,["This is a rumor",1])]]));
+            spawnFull(
+                S + 1, E, lists:append([L, [spawn(server, initiateFull, ["This is a rumor", 1])]])
+            );
         false ->
             L
     end.
 
-spawnLine(S,E,L)->
-    case S=<E of
+spawnLine(S, E, L) ->
+    case S =< E of
         true ->
-            spawnLine(S+1,E,lists:append([L,[spawn(server,initiateLine,["This is a rumor",1])]]));
+            spawnLine(
+                S + 1, E, lists:append([L, [spawn(server, initiateLine, ["This is a rumor", 1])]])
+            );
         false ->
             L
     end.
 
-initiateFull(Rumor,Count) ->
+spawn2D(S, E, NumCols, L) ->
+    case S =< E of
+        true ->
+            spawn2D(
+                S + 1,
+                E,
+                NumCols,
+                lists:append([L, [spawn(server, initiate2D, ["This is a rumor", 1, NumCols])]])
+            );
+        false ->
+            L
+    end.
+
+initiateFull(Rumor, Count) ->
     receive
-        {receiveRumour,List,NumNodes} ->
-            io:format("~p ~p ~n",[self(),Rumor]),
-            io:format("~p Count: ~n",[Count]),
+        {receiveRumour, List, NumNodes} ->
+            io:format("~p ~p ~n", [self(), Rumor]),
+            io:format("~p Count: ~n", [Count]),
             if
-                Count==5->
+                Count == 5 ->
                     processKiller(self());
-                true->
-                    spreadFullRumor(length(List),NumNodes,List,Rumor,self()),
-                    initiateFull(Rumor,Count+1)
+                true ->
+                    spreadFullRumor(length(List), NumNodes, List, Rumor, self()),
+                    initiateFull(Rumor, Count + 1)
             end;
         stop ->
-            io:format("~p Stopping~n",[self()])
+            io:format("~p Stopping~n", [self()])
     end.
 
-initiateLine(Rumor,Count)->
+initiateLine(Rumor, Count) ->
     receive
-        {receiveRumour,List,NumNodes} ->
-            io:format("~p ~p ~n",[self(),Rumor]),
-            io:format("~p Count: ~n",[Count]),
+        {receiveRumour, List} ->
+            io:format("~p ~p ~n", [self(), Rumor]),
+            io:format("~p Count: ~n", [Count]),
             if
-                Count==5->
+                Count == 5 ->
                     processKiller(self());
-                true->
-                    spreadLineRumor(length(List),List,Rumor,self()),
-                    initiateLine(Rumor,Count+1)
+                true ->
+                    spreadLineRumor(List, self()),
+                    initiateLine(Rumor, Count + 1)
             end;
-        stop->
-            io:format("~p Stopping~n",[self()])
+        stop ->
+            io:format("~p Stopping~n", [self()])
     end.
 
-spreadFullRumor(Nodes,NumNodes,List,Rumor,CurrPID)->
-    case NumNodes>0 of
-        true->
-            case checkIfEqual(CurrPID,nth(NumNodes,List)) of
-                true->
-                    spreadFullRumor(Nodes,NumNodes-1,List,Rumor,CurrPID);
-                false->
-                    nth(NumNodes,List) ! {receiveRumour,List,Nodes},
-                    spreadFullRumor(Nodes,NumNodes-1,List,Rumor,CurrPID)
+initiate2D(Rumor, Count, NumCols) ->
+    receive
+        {receiveRumour, List} ->
+            io:format("~p ~p ~n", [self(), Rumor]),
+            io:format("~p Count: ~n", [Count]),
+            if
+                Count == 5 ->
+                    processKiller(self());
+                true ->
+                    spread2DRumor(List, self(), NumCols),
+                    initiate2D(Rumor, Count + 1, NumCols)
             end;
-        false->
+        stop ->
+            io:format("~p Stopping~n", [self()])
+    end.
+
+spreadFullRumor(Nodes, NumNodes, List, Rumor, CurrPID) ->
+    case NumNodes > 0 of
+        true ->
+            case checkIfEqual(CurrPID, nth(NumNodes, List)) of
+                true ->
+                    spreadFullRumor(Nodes, NumNodes - 1, List, Rumor, CurrPID);
+                false ->
+                    nth(NumNodes, List) ! {receiveRumour, List, Nodes},
+                    spreadFullRumor(Nodes, NumNodes - 1, List, Rumor, CurrPID)
+            end;
+        false ->
             ""
     end.
 
-spreadLineRumor(Nodes,List,Rumor,CurrPID)->
-    case checkIfEqual(CurrPID,nth(1,List)) of
-        true->
-            nth(2,List) ! {receiveRumour,List,Nodes};
-        false->
-            case checkIfEqual(CurrPID,lists:last(List)) of
-                true->
-                    nth(length(List)-1,List) ! {receiveRumour,List,Nodes};
-                false->
-                    nth(string:str(List,[CurrPID])+1,List) ! {receiveRumour,List,Nodes},
-                    nth(string:str(List,[CurrPID])-1,List) ! {receiveRumour,List,Nodes}
+spreadLineRumor(List, CurrPID) ->
+    case checkIfEqual(CurrPID, nth(1, List)) of
+        true ->
+            nth(2, List) ! {receiveRumour, List};
+        false ->
+            case checkIfEqual(CurrPID, lists:last(List)) of
+                true ->
+                    nth(length(List) - 1, List) ! {receiveRumour, List};
+                false ->
+                    nth(string:str(List, [CurrPID]) + 1, List) ! {receiveRumour, List},
+                    nth(string:str(List, [CurrPID]) - 1, List) ! {receiveRumour, List}
             end
     end.
 
-checkIfEqual(PID1,PID2)->
+spread2DRumor(List, CurrPID, NumCols) ->
+    Index = string:str(List, [CurrPID]),
+    case Index + 3 =< length(List) of
+        true ->
+            nth(Index + 3, List) ! {receiveRumour, List};
+        false ->
+            ""
+    end,
+    case Index - 3 >= 1 of
+        true ->
+            nth(Index - 3, List) ! {receiveRumour, List};
+        false ->
+            ""
+    end,
+    case Index rem NumCols == 1 of
+        true ->
+            case Index + 1 =< length(List) of
+                true ->
+                    nth(Index + 1, List) ! {receiveRumour, List};
+                false ->
+                    ""
+            end;
+        false ->
+            case Index rem NumCols == 0 of
+                true ->
+                    case Index - 1 >= 1 of
+                        true ->
+                            nth(Index - 1, List) ! {receiveRumour, List};
+                        false ->
+                            ""
+                    end;
+                false ->
+                    nth(Index + 1, List) ! {receiveRumour, List},
+                    nth(Index - 1, List) ! {receiveRumour, List}
+            end
+    end.
+
+checkIfEqual(PID1, PID2) ->
     if
-        PID1==PID2 ->
+        PID1 == PID2 ->
             true;
-        true->
+        true ->
             false
     end.
 
