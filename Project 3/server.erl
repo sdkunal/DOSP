d3eb012fa5726810fa25f24e0240e0b1e2183784
Aakash Anil Naik <89472838+aakash-anil-naik@users.noreply.github.com>
2022-10-22 11:56:31 -0400

    Updates server.erl
    Created map of hex keys to PIDs

--- Project 3/server.erl
+++ Project 3/server.erl
@@ -1,16 +1,30 @@
 -module(server).
 -import(lists, [nth/2, seq/2, append/1, last/1, sort/1]).
--export([start/2, createList/2, spawnActors/3, encrypt/0, initiate/0]).
+-export([
+    start/2,
+    createList/2,
+    createdEncryptedList/3,
+    spawnActors/3,
+    encryptPIDs/4,
+    encrypt/1,
+    initiate/0
+]).
 
 start(NumNodes, NumRequests) ->
     List = createList(1, NumNodes),
-    ListB = sort(List),
-    io:format("List: ~p~n", [ListB]),
+    EncryptedList = createdEncryptedList(1, NumNodes, List),
+    SortedList = sort(List),
+    % SortedEncryptedList = sort(EncryptedList),
+    io:format("Map: ~p~n", [maps:from_list(EncryptedList)]),
+    io:format("List: ~p~n", [SortedList]),
+    % io:format("Encrypted List: ~p~n", [SortedEncryptedList]),
     io:format("Number of requests: ~p~n", [NumRequests]).
-% nth(1, List) ! {receiveRumour, List, NumRequests}.
 
 createList(S, E) ->
     spawnActors(S, E, []).
+
+createdEncryptedList(S, E, List) ->
+    encryptPIDs(S, E, List, []).
 
 spawnActors(S, E, L) ->
     case S =< E of
@@ -18,16 +32,24 @@ spawnActors(S, E, L) ->
             spawnActors(
                 S + 1,
                 E,
-                append([L, [encrypt()]])
+                append([L, [spawn(server, initiate, [])]])
             );
         false ->
             L
     end.
 
-encrypt() ->
+encryptPIDs(S, E, List, L) ->
+    case S =< E of
+        true ->
+            encryptPIDs(S + 1, E, List, append([L, [{encrypt(nth(S, List)), nth(S, List)}]]));
+        false ->
+            L
+    end.
+
+encrypt(PID) ->
     EncryptedStr = [
         element(C + 1, {$0, $1, $2, $3, $4, $5, $6, $7, $8, $9, $A, $B, $C, $D, $E, $F})
-     || <<C:4>> <= crypto:hash(sha256, pid_to_list(spawn(server, initiate, [])))
+     || <<C:4>> <= crypto:hash(sha, pid_to_list(PID))
     ],
     EncryptedStr.
 
