54483e40c368548ac8fc474cb433ea6bd36f5245
Aakash Anil Naik <89472838+aakash-anil-naik@users.noreply.github.com>
2022-12-01 18:49:13 -0500

    added retweeting and follow new users
    added retweeting and follow new users

--- Project 4/client.erl
+++ Project 4/client.erl
@@ -7,12 +7,14 @@ initiateUser(Uid, PPid, NumNodes) ->
     assignFollowers(Uid, PPid, NumNodes).
 
 startSimulation(Actor_List, PPid, S) ->
-    Ops_List = ["tweet", "go_offline", "go_online", "follow","query_hashtags","get_feed"],
-    HashTagList=["tag1","tag2","tag3","tag4","tag5"],
+    Ops_List = [
+        "tweet", "go_offline", "go_online", "follow", "query_hashtags", "get_feed", "retweet"
+    ],
+    HashTagList = ["tag1", "tag2", "tag3", "tag4", "tag5"],
     % io:format("starting simulation: ~n", []),
     case S =< 20 of
         true ->
-            NumNodes=length(Actor_List),
+            NumNodes = length(Actor_List),
             RandomUid = rand:uniform(NumNodes),
             Op_idx = rand:uniform(length(Ops_List)),
             Operation = lists:nth(Op_idx, Ops_List),
@@ -20,8 +22,8 @@ startSimulation(Actor_List, PPid, S) ->
             case Operation == "tweet" of
                 true ->
                     io:format("Uid ~p is tweeting~n", [RandomUid]),
-                    goOnline(PPid,RandomUid),
-                    makeTweet(PPid,RandomUid,NumNodes);
+                    goOnline(PPid, RandomUid),
+                    makeTweet(PPid, RandomUid, NumNodes);
                 false ->
                     case Operation == "go_offline" of
                         true ->
@@ -33,19 +35,25 @@ startSimulation(Actor_List, PPid, S) ->
                                 false ->
                                     case Operation == "follow" of
                                         true ->
-                                            io:format("Follow people~n", []);
+                                            UidToFollow = rand:uniform(NumNodes),
+                                            followUser(NumNodes, UidToFollow, RandomUid, PPid);
                                         false ->
-                                            case Operation=="query_hashtags" of
-                                                true->
-                                                    SelectHashTag=rand:uniform(5),
-                                                    Hash=lists:nth(SelectHashTag,HashTagList),
-                                                    query_hashtags(Hash,PPid);
-                                                false->
-                                                    case Operation=="get_feed" of
-                                                        true->
-                                                            get_feed(RandomUid,PPid);
-                                                        false->
-                                                            ""
+                                            case Operation == "query_hashtags" of
+                                                true ->
+                                                    SelectHashTag = rand:uniform(5),
+                                                    Hash = lists:nth(SelectHashTag, HashTagList),
+                                                    query_hashtags(Hash, PPid);
+                                                false ->
+                                                    case Operation == "get_feed" of
+                                                        true ->
+                                                            get_feed(RandomUid, PPid);
+                                                        false ->
+                                                            case Operation == "retweet" of
+                                                                true ->
+                                                                    reTweet(RandomUid, PPid);
+                                                                false ->
+                                                                    ""
+                                                            end
                                                     end
                                             end
                                     end
@@ -57,17 +65,27 @@ startSimulation(Actor_List, PPid, S) ->
             ""
     end.
 
-get_feed(Uid,PPid)->
-    PPid ! {get_feed,Uid}.
+reTweet(Uid, PPid) ->
+    PPid ! {retweet, Uid}.
 
-query_hashtags(Hash,PPid)->
-    PPid ! {display_hashtags,Hash}.
+followUser(NumNodes, FollowUid, Uid, PPid) ->
+    case FollowUid == Uid of
+        true ->
+            NewUidToFollow = rand:uniform(NumNodes),
+            followUser(NumNodes, NewUidToFollow, Uid, PPid);
+        false ->
+            PPid ! {addFollowers, FollowUid, Uid}
+    end.
 
+get_feed(Uid, PPid) ->
+    PPid ! {get_feed, Uid}.
 
-makeTweet(PPid,Uid,NumNodes)->
-    Tweet=generateRandomTweets(Uid,PPid,NumNodes),
-    PPid ! {postTweet,Uid,Tweet}.
+query_hashtags(Hash, PPid) ->
+    PPid ! {display_hashtags, Hash}.
 
+makeTweet(PPid, Uid, NumNodes) ->
+    Tweet = generateRandomTweets(Uid, PPid, NumNodes),
+    PPid ! {postTweet, Uid, Tweet}.
 
 goOffline(PPid, Uid) ->
     PPid ! {go_offline, Uid}.
@@ -85,20 +103,20 @@ assignFollowers(Uid, PPid, NumNodes) ->
 
 generateRandomTweets(Uid, PPid, NumNodes) ->
     WordList = ["one", "life", "one", "love", "one", "chance", "covfefe"],
-    HashTagList=["tag1","tag2","tag3","tag4","tag5"],
-    ChooseHashTag=rand:uniform(2),
+    HashTagList = ["tag1", "tag2", "tag3", "tag4", "tag5"],
+    ChooseHashTag = rand:uniform(2),
     NumWords = rand:uniform(7),
     List = [rand:uniform(NumWords) || _ <- lists:seq(1, NumWords)],
     Length = length(List),
     String = generateTweet(WordList, "", 1, Length, List),
-    case ChooseHashTag==1 of
-        true->
-            SelectHashTag=rand:uniform(5),
-            Hash=lists:nth(SelectHashTag,HashTagList),
-            HashTag=string:concat("#",Hash),
-            String2=string:concat(String,HashTag),
+    case ChooseHashTag == 1 of
+        true ->
+            SelectHashTag = rand:uniform(5),
+            Hash = lists:nth(SelectHashTag, HashTagList),
+            HashTag = string:concat("#", Hash),
+            String2 = string:concat(String, HashTag),
             String2;
-        false->
+        false ->
             String
     end.
 
--- Project 4/server.erl
+++ Project 4/server.erl
@@ -11,7 +11,9 @@
     replacenth/3,
     takeOffline/2
 ]).
--record(user, {id, followers = [], following = [], tweet = [], feed=[],mentions = [], status = false}).
+-record(user, {
+    id, followers = [], following = [], tweet = [], feed = [], mentions = [], status = false
+}).
 
 start(NumNodes, NumTweets) ->
     io:format("Num Nodes: ~p NumTweets: ~p ~n", [NumNodes, NumTweets]),
@@ -20,99 +22,153 @@ start(NumNodes, NumTweets) ->
     io:format("Actor_List: ~p ~n", [Actor_List]),
     % io:format("Self: ~p ~n",[self()]),
     Record_List = createList(1, NumNodes),
-    HashTags=#{"tag1"=>[],"tag2"=>[],"tag3"=>[],"tag4"=>[],"tag5"=>[]},
+    HashTags = #{"tag1" => [], "tag2" => [], "tag3" => [], "tag4" => [], "tag5" => []},
     spawn(client, startSimulation, [Actor_List, self(), 1]),
     % io:format("List: ~p ~n",[List]),
-    simulator(Record_List,HashTags).
+    simulator(Record_List, HashTags).
 % P=#user{id=1,followers=[1,2,3],following=[4,5],tweet=["abc","xyz"],mentions=["mention1","mention2"],status=true},
 
 generate(S, E, PPid) ->
     generateActors(S, E, [], PPid).
 
-simulator(List,HashTags) ->
+simulator(List, HashTags) ->
     receive
-        {addFollowers} ->
-            io:format("in simulator: ~p ~n", [self()]),
-            simulator(List,HashTags);
+        {addFollowers, FollowUid, Uid} ->
+            % io:format("in simulator: ~p ~n", [self()]),
+            P = nth(Uid, List),
+            FollowerList = P#user.followers,
+            CheckMember = lists:member(FollowUid, FollowerList),
+            case CheckMember == false of
+                true ->
+                    % io:format("List before adding new followers: ~p ~n", [List]),
+                    List1 = addNewFollower(Uid, FollowUid, List),
+                    List2 = addNewFollowing(Uid, FollowUid, List1),
+                    % io:format("List after adding new followers and following: ~p ~n", [List2]),
+                    simulator(List2, HashTags);
+                false ->
+                    simulator(List, HashTags)
+            end;
         {makeOnline, Uid, Pid} ->
             io:format("Actor ~p is in online state with PID ~p ~n", [Uid, Pid]),
             List1 = takeOnline(List, Uid),
-            simulator(List1,HashTags);
+            simulator(List1, HashTags);
         {setFollowers, Uid, FollowerList} ->
             io:format("Followers ~p assigned to ~p ~n", [FollowerList, Uid]),
             List1 = addFollowers(Uid, FollowerList, List),
-            % io:format("List with followers: ~p ~n",[List1]),
-            % List2=addToFollowing(Uid,FollowerList,List1),
             Length = length(FollowerList),
             List2 = addToFollowing(Uid, FollowerList, List1, 1, Length),
             %io:format("Final List: ~p ~n",[List2]),
-            simulator(List2,HashTags);
+            simulator(List2, HashTags);
         {go_offline, Uid} ->
             List1 = takeOffline(List, Uid),
             % io:format("After offline List: ~p ~n", [List1]),
-            simulator(List1,HashTags);
+            simulator(List1, HashTags);
         {go_online, Uid} ->
             List1 = setOnline(List, Uid),
             % io:format("After offline List: ~p ~n", [List1]),
-            simulator(List1,HashTags);
-        {postTweet,Uid,Tweet}->
-            List1=postTweet(List,Uid,Tweet),
-            P=nth(Uid,List1),
-            FollowerList=P#user.followers,
-            Length=length(FollowerList),
-            List2=distributeTweet(List1,Uid,Tweet,FollowerList,1,Length),
-            ContainsHashTag=string:chr(Tweet,$#),
-            case ContainsHashTag=/=0 of
-                true->
-                    Start=ContainsHashTag+1,
-                    End=ContainsHashTag+4,
-                    Tag=string:substr(Tweet,Start,End),
-                    TagList=maps:get(Tag,HashTags),
-                    TagList2=lists:append(TagList,[Tweet]),
-                    Map2=maps:put(Tag,TagList2,HashTags),
+            simulator(List1, HashTags);
+        {postTweet, Uid, Tweet} ->
+            List1 = postTweet(List, Uid, Tweet),
+            P = nth(Uid, List1),
+            FollowerList = P#user.followers,
+            Length = length(FollowerList),
+            List2 = distributeTweet(List1, Uid, Tweet, FollowerList, 1, Length),
+            ContainsHashTag = string:chr(Tweet, $#),
+            case ContainsHashTag =/= 0 of
+                true ->
+                    Start = ContainsHashTag + 1,
+                    End = ContainsHashTag + 4,
+                    Tag = string:substr(Tweet, Start, End),
+                    TagList = maps:get(Tag, HashTags),
+                    TagList2 = lists:append(TagList, [Tweet]),
+                    Map2 = maps:put(Tag, TagList2, HashTags),
                     io:format("Hashtags after update: ~p ~n", [Map2]),
-                    simulator(List2,Map2);
-                false->
+                    simulator(List2, Map2);
+                false ->
                     io:format("Hashtags without update: ~p ~n", [HashTags]),
-                    simulator(List2,HashTags)
+                    simulator(List2, HashTags)
             end;
-            % io:format("After tweeting List: ~p ~n", [List2]),
-        {display_hashtags,Hash}->
-            TweetList=maps:get(Hash,HashTags),
-            io:format("Tweets with ~p hashtags: ~p ~n", [Hash,TweetList]),
-            simulator(List,HashTags);
-        {get_feed,Uid}->
-            Feed=displayFeed(Uid,List),
-            io:format("Displaying feed for ~p user: ~p~n", [Uid,Feed]),
-            simulator(List,HashTags)
+        {display_hashtags, Hash} ->
+            TweetList = maps:get(Hash, HashTags),
+            io:format("Tweets with ~p hashtags: ~p ~n", [Hash, TweetList]),
+            simulator(List, HashTags);
+        {get_feed, Uid} ->
+            Feed = displayFeed(Uid, List),
+            io:format("Displaying feed for ~p user: ~p~n", [Uid, Feed]),
+            simulator(List, HashTags);
+        {retweet, Uid} ->
+            P = nth(Uid, List),
+            FeedList = P#user.feed,
+            FeedListLength = length(FeedList),
+            case FeedListLength =/= 0 of
+                true ->
+                    RandTweetIdx = rand:uniform(FeedListLength),
+                    RandTweet = nth(RandTweetIdx, FeedList),
+                    List1 = reTweeting(Uid, List, RandTweet),
+                    FollowerList = P#user.followers,
+                    Length = length(FollowerList),
+                    List2 = distributeTweet(List1, Uid, RandTweet, FollowerList, 1, Length),
+                    ContainsHashTag = string:chr(RandTweet, $#),
+                    case ContainsHashTag =/= 0 of
+                        true ->
+                            Start = ContainsHashTag + 1,
+                            End = ContainsHashTag + 4,
+                            Tag = string:substr(RandTweet, Start, End),
+                            TagList = maps:get(Tag, HashTags),
+                            TagList2 = lists:append(TagList, [RandTweet]),
+                            Map2 = maps:put(Tag, TagList2, HashTags),
+                            io:format("Hashtags after update: ~p ~n", [Map2]),
+                            simulator(List2, Map2);
+                        false ->
+                            io:format("Hashtags without update: ~p ~n", [HashTags]),
+                            simulator(List2, HashTags)
+                    end;
+                false ->
+                    io:format("No tweets in feed to retweet~n", []),
+                    simulator(List, HashTags)
+            end
     end.
 
+reTweeting(Uid, List, RandTweet) ->
+    P = nth(Uid, List),
+    TweetList = P#user.tweet,
+    P1 = P#user{tweet = lists:append(TweetList, [RandTweet])},
+    replacenth(List, Uid, P1).
 
-displayFeed(Uid,List)->
-    P=nth(Uid,List),
-    Feed=P#user.feed,
-    Feed.
+addNewFollowing(Uid, FollowUid, List) ->
+    P = nth(FollowUid, List),
+    FollowingList = P#user.following,
+    P1 = P#user{following = lists:append(FollowingList, [Uid])},
+    replacenth(List, FollowUid, P1).
 
-distributeTweet(List,Uid,Tweet,FollowerList,S,Length)->
-    case S=<Length of
-        true->
+addNewFollower(Uid, FollowUid, List) ->
+    P = nth(Uid, List),
+    FollowerList = P#user.followers,
+    P1 = P#user{followers = lists:append(FollowerList, [FollowUid])},
+    replacenth(List, Uid, P1).
+
+displayFeed(Uid, List) ->
+    P = nth(Uid, List),
+    P#user.feed.
+
+distributeTweet(List, Uid, Tweet, FollowerList, S, Length) ->
+    case S =< Length of
+        true ->
             Curr = nth(S, FollowerList),
             P = nth(Curr, List),
             FeedList = P#user.feed,
             P1 = P#user{feed = lists:append(FeedList, [Tweet])},
             List2 = replacenth(List, Curr, P1),
-            distributeTweet(List2,Uid,Tweet,FollowerList,S+1,Length);
+            distributeTweet(List2, Uid, Tweet, FollowerList, S + 1, Length);
         false ->
             List
     end.
 
-postTweet(List,Uid,Tweet)->
-    P=nth(Uid,List),
-    TweetList=P#user.tweet,
-    P1=P#user{tweet=lists:append(TweetList,[Tweet])},
-    List2=replacenth(List,Uid,P1),
-    List2.
-
+postTweet(List, Uid, Tweet) ->
+    P = nth(Uid, List),
+    TweetList = P#user.tweet,
+    P1 = P#user{tweet = lists:append(TweetList, [Tweet])},
+    replacenth(List, Uid, P1).
 
 takeOffline(List, Uid) ->
     P = lists:nth(Uid, List),
@@ -195,7 +251,7 @@ createNewList(S, E, L) ->
                             followers = [],
                             following = [],
                             tweet = [],
-                            feed =[],
+                            feed = [],
                             mentions = [],
                             status = false
                         }
