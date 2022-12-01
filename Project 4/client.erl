5174497628718ab50ab849c2f7fe11352c5c8c20
Aakash Anil Naik <89472838+aakash-anil-naik@users.noreply.github.com>
2022-12-01 16:29:06 -0500

    take users online and offline
    take users online and offline

--- Project 4/client.erl
+++ Project 4/client.erl
@@ -1,59 +1,85 @@
 -module(client).
--import(server,[start/2]).
--export([initiateUser/3,startSimulation/2]).
+-import(server, [start/2]).
+-export([initiateUser/3, startSimulation/3, generateRandomTweets/3, generateTweet/5]).
 
-initiateUser(Uid,PPid,NumNodes)->
-    PPid ! {makeOnline,Uid,self()},
-    assignFollowers(Uid,PPid,NumNodes).
-    % generateRandomTweets(Uid,PPid,NumNodes).
+initiateUser(Uid, PPid, NumNodes) ->
+    PPid ! {makeOnline, Uid, self()},
+    assignFollowers(Uid, PPid, NumNodes).
 
-startSimulation(Actor_List,PPid)->
-    Ops_List=["tweet","go_offline","go_online","follow"],
-    io:format("starting simulation: ~n",[]),
-    Op_idx=rand:uniform(4),
-    Operation=lists:nth(Op_idx,Ops_List),
-    io:format("Operation selected: ~p ~n",[Operation]),
-    goOffline(PPid,2).
+startSimulation(Actor_List, PPid, S) ->
+    Ops_List = ["tweet", "go_offline", "go_online", "follow"],
+    % io:format("starting simulation: ~n", []),
+    case S =< 10 of
+        true ->
+            RandomUid = rand:uniform(length(Actor_List)),
+            Op_idx = rand:uniform(4),
+            Operation = lists:nth(Op_idx, Ops_List),
+            io:format("Operation selected: ~p ~n", [Operation]),
+            case Operation == "tweet" of
+                true ->
+                    io:format("Make tweets~n", []);
+                false ->
+                    case Operation == "go_offline" of
+                        true ->
+                            goOffline(PPid, RandomUid);
+                        false ->
+                            case Operation == "go_online" of
+                                true ->
+                                    goOnline(PPid, RandomUid);
+                                false ->
+                                    case Operation == "follow" of
+                                        true ->
+                                            io:format("Follow people~n", []);
+                                        false ->
+                                            ""
+                                    end
+                            end
+                    end
+            end,
+            startSimulation(Actor_List, PPid, S + 1);
+        false ->
+            ""
+    end.
 
-goOffline(PPid,Uid)->
-    PPid ! {go_offline,Uid}.
+goOffline(PPid, Uid) ->
+    PPid ! {go_offline, Uid}.
 
-assignFollowers(Uid,PPid,NumNodes)->
-    NumFollowers=rand:uniform(NumNodes-2)+1,
-    List=[rand:uniform(NumNodes) || _ <- lists:seq(1, NumFollowers)],
-    List2=checkList(List,Uid),
-    Set=ordsets:from_list(List2),
-    FollowerList=ordsets:to_list(Set),
-    PPid ! {setFollowers,Uid,FollowerList}.
+goOnline(PPid, Uid) ->
+    PPid ! {go_online, Uid}.
 
-generateRandomTweets(Uid,PPid,NumNodes)->
-    WordList=["one","life","one","love","one","chance","covfefe"],
-    NumWords=rand:uniform(7),
-    List=[rand:uniform(NumWords) || _ <- lists:seq(1, NumWords)],
-    Length=length(List),
-    String=generateTweet(WordList,"",1,Length,List),
-    io:format("Generated String: ~p ~n",[String]).
+assignFollowers(Uid, PPid, NumNodes) ->
+    NumFollowers = rand:uniform(NumNodes - 2) + 1,
+    List = [rand:uniform(NumNodes) || _ <- lists:seq(1, NumFollowers)],
+    List2 = checkList(List, Uid),
+    Set = ordsets:from_list(List2),
+    FollowerList = ordsets:to_list(Set),
+    PPid ! {setFollowers, Uid, FollowerList}.
 
-generateTweet(WordList,String,S,Length,List)->
-    case S=<Length of
-        true->
-            Curr=lists:nth(S,List),
-            Str=lists:nth(Curr,WordList),
-            String2=string:concat(String,Str),
-            String3=string:concat(String2," "),
-            generateTweet(WordList,String3,S+1,Length,List);
-        false->
+generateRandomTweets(Uid, PPid, NumNodes) ->
+    WordList = ["one", "life", "one", "love", "one", "chance", "covfefe"],
+    NumWords = rand:uniform(7),
+    List = [rand:uniform(NumWords) || _ <- lists:seq(1, NumWords)],
+    Length = length(List),
+    String = generateTweet(WordList, "", 1, Length, List),
+    io:format("Generated String: ~p ~n", [String]).
+
+generateTweet(WordList, String, S, Length, List) ->
+    case S =< Length of
+        true ->
+            Curr = lists:nth(S, List),
+            Str = lists:nth(Curr, WordList),
+            String2 = string:concat(String, Str),
+            String3 = string:concat(String2, " "),
+            generateTweet(WordList, String3, S + 1, Length, List);
+        false ->
             String
     end.
 
-
-checkList(List,Uid)->
-    Bool=lists:member(Uid,List),
+checkList(List, Uid) ->
+    Bool = lists:member(Uid, List),
     case Bool of
-        true->
-            lists:delete(Uid,List); 
-        false->
+        true ->
+            lists:delete(Uid, List);
+        false ->
             List
     end.
-
-
--- Project 4/server.erl
+++ Project 4/server.erl
@@ -1,86 +1,121 @@
 -module(server).
--import(client,[initiateUser/3,startSimulation/2]).
--import(lists,[nth/2,append/1]).
--export([start/2,generateActors/4,createList/2,createNewList/3,simulator/1,takeOnline/2,replacenth/3]).
--record(user, {id,followers=[],following=[],tweet=[],mentions=[],status=false}). 
+-import(client, [initiateUser/3, startSimulation/3, generateRandomTweets/3, generateTweet/5]).
+-import(lists, [nth/2, append/1]).
+-export([
+    start/2,
+    generateActors/4,
+    createList/2,
+    createNewList/3,
+    simulator/1,
+    takeOnline/2,
+    replacenth/3,
+    takeOffline/2
+]).
+-record(user, {id, followers = [], following = [], tweet = [], mentions = [], status = false}).
 
-start(NumNodes,NumTweets)->
-    io:format("Num Nodes: ~p NumTweets: ~p ~n",[NumNodes,NumTweets]),
+start(NumNodes, NumTweets) ->
+    io:format("Num Nodes: ~p NumTweets: ~p ~n", [NumNodes, NumTweets]),
     % Actor_List=generateActors(1,NumNodes,self()),
-    Actor_List=generate(1,NumNodes,self()),
-    io:format("Actor_List: ~p ~n",[Actor_List]),
+    Actor_List = generate(1, NumNodes, self()),
+    io:format("Actor_List: ~p ~n", [Actor_List]),
     % io:format("Self: ~p ~n",[self()]),
     Record_List = createList(1, NumNodes),
-    spawn(client,startSimulation,[Actor_List,self()]),
+    spawn(client, startSimulation, [Actor_List, self(), 1]),
     % io:format("List: ~p ~n",[List]),
     simulator(Record_List).
-    % P=#user{id=1,followers=[1,2,3],following=[4,5],tweet=["abc","xyz"],mentions=["mention1","mention2"],status=true},
+% P=#user{id=1,followers=[1,2,3],following=[4,5],tweet=["abc","xyz"],mentions=["mention1","mention2"],status=true},
 
-generate(S,E,PPid)->
-    generateActors(S,E,[],PPid).
+generate(S, E, PPid) ->
+    generateActors(S, E, [], PPid).
 
-simulator(List)->
+simulator(List) ->
     receive
-        {addFollowers}->
-            io:format("in simulator: ~p ~n",[self()]),
+        {addFollowers} ->
+            io:format("in simulator: ~p ~n", [self()]),
             simulator(List);
-        {makeOnline,Uid,Pid}->
-            io:format("Actor ~p is in online state with PID ~p ~n",[Uid,Pid]),
-            List1=takeOnline(List,Uid),
+        {makeOnline, Uid, Pid} ->
+            io:format("Actor ~p is in online state with PID ~p ~n", [Uid, Pid]),
+            List1 = takeOnline(List, Uid),
             simulator(List1);
-        {setFollowers,Uid,FollowerList}->
-            io:format("Followers ~p assigned to ~p ~n",[FollowerList,Uid]),
-            List1=addFollowers(Uid,FollowerList,List),
+        {setFollowers, Uid, FollowerList} ->
+            io:format("Followers ~p assigned to ~p ~n", [FollowerList, Uid]),
+            List1 = addFollowers(Uid, FollowerList, List),
             % io:format("List with followers: ~p ~n",[List1]),
             % List2=addToFollowing(Uid,FollowerList,List1),
-            Length=length(FollowerList),
-            List2=addToFollowing(Uid,FollowerList,List1,1,Length),
+            Length = length(FollowerList),
+            List2 = addToFollowing(Uid, FollowerList, List1, 1, Length),
             %io:format("Final List: ~p ~n",[List2]),
             simulator(List2);
-        {go_offline,Uid}->
-            List1=takeOffline(List,Uid),
-            io:format("After offline List: ~p ~n",[List1]),
+        {go_offline, Uid} ->
+            List1 = takeOffline(List, Uid),
+            % io:format("After offline List: ~p ~n", [List1]),
+            simulator(List1);
+        {go_online, Uid} ->
+            List1 = setOnline(List, Uid),
+            % io:format("After offline List: ~p ~n", [List1]),
             simulator(List1)
     end.
 
-takeOffline(List,Uid)->
-    P=nth(Uid,List),
-    P1 = P#user{status = false},
-    replacenth(List,Uid,P1).
-
-addToFollowing(Uid,FollowerList,List,S,Length)->
-    case S=<Length of
-        true->
-            Curr=nth(S,FollowerList),
-            P=nth(Curr,List),
-            TempList=P#user.following,
-            P1=P#user{following = lists:append(TempList,[Uid])},
-            List2=replacenth(List,Curr,P1),
-            addToFollowing(Uid,FollowerList,List2,S+1,Length);
-        false->
+takeOffline(List, Uid) ->
+    P = lists:nth(Uid, List),
+    Bool = P#user.status,
+    case Bool == true of
+        true ->
+            P1 = P#user{status = false},
+            replacenth(List, Uid, P1);
+        false ->
+            io:format("User was already offline~n", []),
             List
     end.
 
-addFollowers(Uid,FollowerList,List)->
-    P=nth(Uid,List),
+setOnline(List, Uid) ->
+    P = lists:nth(Uid, List),
+    Bool = P#user.status,
+    case Bool == false of
+        true ->
+            P1 = P#user{status = true},
+            replacenth(List, Uid, P1);
+        false ->
+            io:format("User was already online~n", []),
+            List
+    end.
+
+addToFollowing(Uid, FollowerList, List, S, Length) ->
+    case S =< Length of
+        true ->
+            Curr = nth(S, FollowerList),
+            P = nth(Curr, List),
+            TempList = P#user.following,
+            P1 = P#user{following = lists:append(TempList, [Uid])},
+            List2 = replacenth(List, Curr, P1),
+            addToFollowing(Uid, FollowerList, List2, S + 1, Length);
+        false ->
+            List
+    end.
+
+addFollowers(Uid, FollowerList, List) ->
+    P = nth(Uid, List),
     P1 = P#user{followers = FollowerList},
-    replacenth(List,Uid,P1).
+    replacenth(List, Uid, P1).
 
-takeOnline(List,Uid)->
-    P=nth(Uid,List),
+takeOnline(List, Uid) ->
+    P = nth(Uid, List),
     P1 = P#user{status = true},
-    replacenth(List,Uid,P1).
+    replacenth(List, Uid, P1).
 
-replacenth(L,Index,NewValue) -> 
-    {L1,[_|L2]} = lists:split(Index-1,L),
-    L1++[NewValue|L2].
+replacenth(L, Index, NewValue) ->
+    {L1, [_ | L2]} = lists:split(Index - 1, L),
+    L1 ++ [NewValue | L2].
 
-generateActors(S,E,L,PPid)-> %E is numNodes
-    case S=<E of
-        true->
+%E is numNodes
+generateActors(S, E, L, PPid) ->
+    case S =< E of
+        true ->
             % spawn(client,initiateUser,[S,PPid,E]),
-            generateActors(S+1,E,lists:append([L,[spawn(client,initiateUser,[S,PPid,E])]]),PPid);
-        false->
+            generateActors(
+                S + 1, E, lists:append([L, [spawn(client, initiateUser, [S, PPid, E])]]), PPid
+            );
+        false ->
             io:format("All actors generated ~n"),
             L
     end.
@@ -94,7 +129,19 @@ createNewList(S, E, L) ->
             createNewList(
                 S + 1,
                 E,
-                append([L, [#user{id=S,followers=[],following=[],tweet=[],mentions=[],status=false}]])
+                lists:append([
+                    L,
+                    [
+                        #user{
+                            id = S,
+                            followers = [],
+                            following = [],
+                            tweet = [],
+                            mentions = [],
+                            status = false
+                        }
+                    ]
+                ])
             );
         false ->
             L
